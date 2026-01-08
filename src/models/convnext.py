4cc98cbb26f15f6fdf7722e01326ed09e733334a
Indira Anindha Maharani <indira09maharani@gmail.com>
2026-01-08 13:36:21 +0700

    using pretrained model

--- src/models/convnext.py
+++ src/models/convnext.py
@@ -7,20 +7,6 @@ def build_convnext(num_classes, pretrained=True, freeze_backbone=True):
         pretrained=pretrained
     )
 
-    # in_features = model.get_classifier().in_features
-
-    # model.head = nn.Sequential(
-    #     nn.Dropout(p=0.3),
-    #     nn.Linear(in_features, num_classes)
-    # )
-
-    # if freeze_backbone:
-    #     for name, param in model.named_parameters():
-    #         if "head" not in name:
-    #             param.requires_grad = False
-
-    # return model
-
     model.reset_classifier(num_classes)
 
     if freeze_backbone:
